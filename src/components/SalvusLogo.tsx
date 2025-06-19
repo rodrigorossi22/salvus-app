50816638a6504b4be7862a2de132de39de3b4585
Rodrigo Rossi <rodrigoliveira.rossi@gmail.com>
2025-06-19 16:50:50 -0300

    Atualização automática

--- src/components/SalvusLogo.tsx
+++ src/components/SalvusLogo.tsx
@@ -1,11 +1,10 @@
-
 import React from 'react';
 
 const SalvusLogo = ({ className = "w-64 h-auto" }: { className?: string }) => {
   return (
     <div className={`${className} flex flex-col items-center justify-center`}>
       <img 
-        src="/lovable-uploads/d47e2f8d-f4db-4027-abda-d19aef571774.png" 
+        src="./lovable-uploads/d47e2f8d-f4db-4027-abda-d19aef571774.png" 
         alt="Clínica Salvus - Medicina e Estética Afetiva" 
         className="w-full h-auto object-contain"
       />
