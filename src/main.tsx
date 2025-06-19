35cf208a6ea9119312be2cab54a3bb69b7fe7418
Rodrigo Rossi <rodrigoliveira.rossi@gmail.com>
2025-06-19 16:35:01 -0300

    Atualiza todos os arquivos do projeto

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,12 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+console.log('Iniciando aplicação Salvus...');
+const rootElement = document.getElementById("root");
+console.log('Elemento root:', rootElement);
+if (rootElement) {
+  createRoot(rootElement).render(<App />);
+  console.log('App renderizado com sucesso!');
+} else {
+  console.error('Elemento root não encontrado!');
+}
